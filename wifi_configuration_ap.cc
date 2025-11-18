cf1e89fd402a0f10409a597cf208d760d7c40556
laride <198868291+laride@users.noreply.github.com>
2025-11-18 22:55:25 +0800

    fix: extend Wi-Fi connection tolerance time on 5G (#24)
    * fix: extend Wi-Fi connection tolerance time on 5G
    
    * Modify WiFi connection wait time in AP configuration
    
    Updated connection wait time from 5 seconds to 10 or 25 seconds.
    
    ---------
    
    Co-authored-by: Xiaoxia <terrence@tenclass.com>

--- wifi_configuration_ap.cc
+++ wifi_configuration_ap.cc
@@ -15,6 +15,7 @@
 #include <cJSON.h>
 #include <esp_smartconfig.h>
 #include "ssid_manager.h"
+#include "sdkconfig.h"
 
 #define TAG "WifiConfigurationAp"
 
@@ -705,8 +706,18 @@ bool WifiConfigurationAp::ConnectToWifi(const std::string &ssid, const std::stri
     }
     ESP_LOGI(TAG, "Connecting to WiFi %s", ssid.c_str());
 
-    // Wait for the connection to complete for 5 seconds
-    EventBits_t bits = xEventGroupWaitBits(event_group_, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdTRUE, pdFALSE, pdMS_TO_TICKS(10000));
+    // Wait for the connection to complete for 10 or 25 seconds
+    EventBits_t bits = xEventGroupWaitBits(
+        event_group_,
+        WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+        pdTRUE,
+        pdFALSE,
+#ifdef CONFIG_SOC_WIFI_SUPPORT_5G
+        pdMS_TO_TICKS(25000)
+#else
+        pdMS_TO_TICKS(10000)
+#endif
+    );
     is_connecting_ = false;
 
     if (bits & WIFI_CONNECTED_BIT) {
